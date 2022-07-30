5549fa7e95aaa1068f07e1ac7fd7e0a099812912
lofti198 <isolar2005@gmail.com>
2022-07-30 19:37:23 +0300

    Just test of updates

--- src/components/VideoCollagePlaylist.js
+++ src/components/VideoCollagePlaylist.js
@@ -1,33 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import YouTube from "react-youtube";
 
 const VideoCollagePlaylist = () => {
   const [player, setPlayer] = useState(null);
-  const [videoId, setVideoId] = useState(() => {
-    return demoPlaylist[0].id;
-  });
+  const [curFragment, setCurFragment] = useState(0);
+  const [videoId, setVideoId] = useState("VcT8puLpNKA");
+  const switchCalledAtLeastOnceRef = useRef(false);
   const opts = {
     //  height: '390',
     width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-
       autoplay: 1,
-      // https://github.com/tjallingt/react-youtube/issues/327
       mute: 0,
     },
   };
 
+  useEffect(() => {
+    console.log("useEffect init");
+    // setVideoId(demoPlaylist[0].id);
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect for player", player);
+    if (switchCalledAtLeastOnceRef.current === false && player) {
+      switchCalledAtLeastOnceRef.current = true;
+      switchNextVideoFragment();
+    }
+  }, [player]);
+
+  // useEffect(() => {
+  //...
+  // }, [videoId]);
+  // useEffect(() => {
+  //   if (
+  //     player &&
+  //     videoId.length > 0 &&
+  //     switchCalledAtLeastOnceRef.current === false
+  //   ) {
+  //     switchCalledAtLeastOnceRef.current = true;
+  //     switchNextVideoFragment();
+  //   } else {
+  //     console.log("useeffect", player, videoId);
+  //   }
+  // }, [player, videoId]);
+
+  const switchNextVideoFragment = () => {
+    console.log(
+      "switchNextVideoFragment",
+      curFragment,
+      demoPlaylist[curFragment].id
+    );
+
+    // setVideoId(demoPlaylist[curFragment].id);
+    console.log(player);
+    player.playVideo();
+    player.seekTo(demoPlaylist[curFragment].start);
+    setTimeout(() => {
+      if (curFragment < demoPlaylist.length - 1) switchNextVideoFragment();
+      else console.log("Cycle End");
+    }, 1000 * (demoPlaylist[curFragment].end - demoPlaylist[curFragment].start));
+
+    setCurFragment((prev) => {
+      if (prev < demoPlaylist.length - 1) return prev + 1;
+      else return 0;
+    });
+  };
+
   const _onReady = (event) => {
+    console.log("onready");
+    //
     setPlayer(event.target);
-    // console.log("_onReady")
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-    //console.log("pauseVideo sent")
   };
 
   return (
     <>
+      Just test of updates
       <YouTube videoId={videoId} opts={opts} onReady={_onReady} />
     </>
   );
